935f6c80faf162dd4d8dd4d84f132dc0cb5c3f15
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-03-05 10:37:48 +0700

    Setup index.php

---
+++ Pertemuan 5 Maret 2026/lk01-hello/htdocs/index.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'db';
+$user = 'root';
+$pass = 'farrel';
+$db_name = 'hello_db';
+$conn = new mysql($host, $user, $pass, $db_name);
+if ($conn->connect_error) {
+    die("<h2>Koneksi DB lu Gagal: " . $conn->connect_error . "</h2>");
+}
+echo "<h1>Hello World!</h1>";
+echo "<p>Database MySQL di Docker terhubung</p>";
+?>
