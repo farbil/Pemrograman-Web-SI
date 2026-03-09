63c44457d94c2417685ea2e6b96142473edfb168
Ahmad Farrel Ash Shiddiqi <farbil232@gmail.com>
2026-03-09 14:39:26 +0700

    Mengubah menjadi huruf kapital

--- Pertemuan 5 Maret 2026/lk01-hello/htdocs/index.php
+++ Pertemuan 5 Maret 2026/lk01-hello/htdocs/index.php
@@ -5,7 +5,7 @@ $pass = 'farrel';
 $db_name = 'hello_db';
 $conn = new mysqli($host, $user, $pass, $db_name);
 if ($conn->connect_error) {
-    die("<h2>Koneksi DB lu Gagal: " . $conn->connect_error . "</h2>");
+    die("<h2>KONEKSI DB LU GAGAL!: " . $conn->connect_error . "</h2>");
 }
 echo "<h1>Hello World!</h1>";
 echo "<p>Database MySQL di Docker terhubung</p>";
